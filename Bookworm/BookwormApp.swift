4056010f10eab9a05e7652b8e218c4fc84ffc5ed
KrasnovYuri <krasnov1yuri@gmail.com>
2024-06-24 10:09:45 +0300

    start project

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Юрий on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
